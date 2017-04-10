ae751690b4093135137fcac8c6bdd377e1253678
Rens Rikkerink <github@ikkerens.com>
2017-04-11 00:38:13 +0200

    Rewrote endpoints to support bucket IDs

--- builder.go
+++ builder.go
@@ -25,7 +25,7 @@ func LoginWithToken(tokenType TokenType, token string) (*Session, error) {
 	session := &Session{tokenType: tokenType, token: token, objects: make(map[Snowflake]interface{})}
 
 	gateway := gatewayGetResponse{}
-	err := session.doHttpGet(EndPointBotGateway, &gateway)
+	err := session.doRequest("GET", EndPointBotGateway()(false), nil, &gateway)
 	if err != nil {
 		return nil, err
 	}
