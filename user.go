02b9d3a7b5f35d65ccee7ea7396ed51842781765
Rens Rikkerink <github@ikkerens.com>
2017-04-21 22:51:39 +0200

    AvatarURL() now returns an empty string if no avatar is present

--- user.go
+++ user.go
@@ -3,5 +3,9 @@ package disgo
 import "fmt"
 
 func (s *User) AvatarURL() string {
+	if s.internal.AvatarHash == "" {
+		return ""
+	}
+
 	return fmt.Sprintf(EndPointUserAvatar, s.internal.ID, s.internal.AvatarHash)
 }
