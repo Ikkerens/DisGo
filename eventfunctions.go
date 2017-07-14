487ee4da011d9f948fd030206819c096584575e2
Rens Rikkerink <rens.rikkerink@eastbridgerfid.nl>
2017-07-14 14:09:54 +0200

    Added Channel() command to MessageCreateEvent

--- eventfunctions.go
+++ eventfunctions.go
@@ -3,3 +3,10 @@ package disgo
 func (e *MessageCreateEvent) Reply(content string) (*Message, error) {
 	return e.session.SendMessage(e.ChannelID(), content)
 }
+
+func (e *MessageCreateEvent) Channel() *Channel {
+	objects.channelLock.RLock()
+	defer objects.channelLock.RUnlock()
+
+	return objects.channels[e.internal.ChannelID]
+}
